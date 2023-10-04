8b412701d5b3701b67aa65472f9a28af81017a9d
abhixyz1 <143863755+abhixyz1@users.noreply.github.com>
2023-10-04 20:26:27 +0700

    Update BookingTiketKereta.java

--- BookingTiketKereta.java
+++ BookingTiketKereta.java
@@ -3,11 +3,16 @@ import java.util.Scanner;
 public class BookingTiketKereta {
     public static void main (String[] args) {
         Scanner sc = new Scanner(System.in);
+        
+        // input
         int jml_tiket, harga_tiket, jml_pembayaran;
+        
         System.out.println("Masukkan jumlah tiket yang dibeli");
         jml_tiket = sc.nextInt();
         System.out.println("Masukkan harga tiket yang dibeli");
         harga_tiket = sc.nextInt();
+
+        // output
         jml_pembayaran = jml_tiket*harga_tiket;
         System.out.println("Total Pembayaran adalah " + jml_pembayaran);
 
